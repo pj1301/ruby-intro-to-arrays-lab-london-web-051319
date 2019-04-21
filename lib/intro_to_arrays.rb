18b8f81066fd68e70f99c790e0f5f23987e7e4d7
James Hayward <james.hayward6@icloud.com>
2019-04-21 20:33:54 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,34 @@
+def instantiate_new_array
+  array = []
+end
+
+def array_with_two_elements
+  array = [
+    "cats",
+    "dogs"
+  ]
+end
+
+def first_element(array)
+  return array[0]
+end
+
+def third_element(array)
+  return array[2]
+end
+
+def last_element(array)
+  return array.last
+end
+
+def first_element_with_array_methods(array)
+  return array.first
+end
+
+def last_element_with_array_methods(array)
+  return array.last
+end
+
+def length_of_array(array)
+  return array.size
+end
